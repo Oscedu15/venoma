9b104937855b3e4d6085ecaccf91605907027a2d
Oscar Garcia <oscaregarcialeon@gmail.com>
2025-09-03 13:01:48 -0400

    feat: latest-sidenav-slider-complete

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Link from "next/link";
+import Slider from "./Slider";
 
 const Latest = () => {
   return (
-    <div>Latest</div>
-  )
-}
+    <section className="mt-24">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-end mb-[72px]">
+          <h4 className="h4 w-max border-b-2">Explore Our Lates Collection</h4>
+          <Link href={"#"} className="link-primary text-accent2 ">
+            View All
+          </Link>
+        </div>
+        <Slider />
+      </div>
+    </section>
+  );
+};
 
-export default Latest
+export default Latest;
--- components/Sidenav.jsx
+++ components/Sidenav.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { Menu } from "lucide-react";
 
 const Sidenav = () => {
   return (
-    <div>Sidenav</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger className="cursor-pointer flex items-center justify-center">
+        <Menu className="text-3xl text-primary" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col h-full pt-16">
+          <SheetHeader>
+            <SheetTitle className="max-w-max mx-auto mb-48">
+              <Link href="#">
+                <Image
+                  src={"/assets/logo-white.svg"}
+                  width={200}
+                  height={200}
+                  alt="Logo"
+                />
+              </Link>
+            </SheetTitle>
+            <SheetDescription className="sr-only">
+              Navigation Menu
+            </SheetDescription>
+          </SheetHeader>
+          {/* nav */}
+          <nav className="flex flex-col items-center gap-12 text-white">
+            <a href="#" className="link-primary hover:text-accent2">
+              Men
+            </a>
+            <a href="#" className="link-primary hover:text-accent2">
+              Women
+            </a>
+            <a href="#" className="link-primary hover:text-accent2">
+              Kids
+            </a>
+            <a href="#" className="link-primary hover:text-accent2">
+              Sale
+            </a>
+          </nav>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default Sidenav
+export default Sidenav;
